70df35becd82b17d690337cd3a567d20541317b9
Justin Fangrad <justin.fangrad@shopify.com>
2018-12-03 16:24:51 -0500

    Add character count

--- src/components/TextField/tests/TextField.test.tsx
+++ src/components/TextField/tests/TextField.test.tsx
@@ -305,6 +305,41 @@ describe('<TextField />', () => {
     });
   });
 
+  describe('characterCount', () => {
+    it('displays number of characters entered in input field', () => {
+      const textField = mountWithAppProvider(
+        <TextField
+          value="test"
+          showCharacterCount
+          label="TextField"
+          id="MyField"
+          onChange={noop}
+        />,
+      );
+
+      const characterCount = textField.find('#MyField-character-counter');
+
+      expect(characterCount.text()).toBe('4');
+    });
+
+    it('displays remaining characters in input field with maxLength', () => {
+      const textField = mountWithAppProvider(
+        <TextField
+          value="test"
+          maxLength={10}
+          showCharacterCount
+          label="TextField"
+          id="MyField"
+          onChange={noop}
+        />,
+      );
+
+      const characterCount = textField.find('#MyField-character-counter');
+
+      expect(characterCount.text()).toBe('6');
+    });
+  });
+
   describe('type', () => {
     it('sets the type on the input', () => {
       const type = shallowWithAppProvider(
